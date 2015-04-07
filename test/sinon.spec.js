3e6ab7c26e6e42e368ac1661934f315b7fb62936
Noam Shemesh <noam@bigpanda.io>
2015-04-07 19:26:10 +0300

    minor refactor

--- test/sinon.spec.js
+++ test/sinon.spec.js
@@ -8,25 +8,6 @@ var NumberFactory = require('../pre/number');
 describe('Expect examples', function () {
   var tested;
 
-  describe('Number', function () {
-    beforeEach(function () {
-      tested = new NumberFactory();
-    });
-
-    it('should start with value 0', function () {
-      expect(tested.get()).to.equal(0);
-    });
-
-    it('should increase value by 1', function () {
-      tested.increase();
-      expect(tested.get()).to.equal(1);
-    });
-
-    it('should return increased value', function () {
-      expect(tested.increase()).to.equal(1);
-    });
-  });
-
   describe('Person', function () {
     var sinon = require('sinon');
 
